5d36dbd17a624adca7e15a72001bca37f8a204f0
jacobxcoder <gillbatesdev@gmail.com>
2024-06-12 22:02:51 +0200

    feat(day): init day view for journal, habits, and more to come

---
+++ src/routes/day/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Button, Textarea } from '$lib/components';
+
+  let journal = '';
+</script>
+
+<Textarea
+  label="Describe your day"
+  rows={6}
+  bind:value={journal}
+  textAreaClass="min-h-20"
+  placeholder="What did you eat? How did you feel? How much did you stress? How was your skin? How was your gut?" />
+
+<Button type="submit" class="btn-primary mt-6 w-full">Save your day</Button>
