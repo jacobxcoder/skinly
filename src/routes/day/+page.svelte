4233a70dd2d1673586859cc126979ee4e8a6ab9a
jacobxcoder <gillbatesdev@gmail.com>
2024-06-13 22:06:17 +0200

    feat(day): init habit component

--- src/routes/day/+page.svelte
+++ src/routes/day/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { Button, Textarea } from '$lib/components';
+  import Habit from './_components/habit.svelte';
 
   let journal = '';
 </script>
@@ -10,5 +11,17 @@
   bind:value={journal}
   textAreaClass="min-h-20"
   placeholder="What did you eat? How did you feel? How much did you stress? How was your skin? How was your gut?" />
+
+<section>
+  <h2 class="text-xl font-bold">Habits</h2>
+  <Habit
+    habit={{
+      id: '1',
+      name: 'Drink 2L of water',
+      description: 'Drink 2 liters of water every day',
+      frequency: 'daily',
+      completed: false
+    }} />
+</section>
 
 <Button type="submit" class="btn-primary mt-6 w-full">Save your day</Button>
---
+++ src/routes/day/_components/habit.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  interface Habit {
+    id: string;
+    name: string;
+    description: string;
+    frequency: string;
+    completed: boolean;
+  }
+
+  export let habit: Habit;
+</script>
+
+<div class="flex items-center justify-between">
+  <div>
+    <h3 class="text-lg font-semibold">{habit.name}</h3>
+    <p class="text-sm text-gray-500">{habit.description}</p>
+  </div>
+
+  <input type="checkbox" bind:checked={habit.completed} />
+</div>
