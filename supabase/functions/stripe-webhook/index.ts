bceeb2d8767d9a5aa4ea250cf5afa85aec60cee1
jacobxcoder <gillbatesdev@gmail.com>
2024-06-20 08:29:25 +0200

    feat(payments): add saving payment status to the db

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,6 +1,7 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import 'https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts';
-import Stripe from 'https://esm.sh/stripe@15.2.0?target=deno';
+import Stripe from 'https://esm.sh/stripe@15.12.0?target=deno';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0?target=deno';
 
 console.log('[supabase functions] Initialized Stripe webhook function.');
 
@@ -11,7 +12,10 @@ const stripe = new Stripe(Deno.env.get('STRIPE_API_KEY') as string, {
 
 const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
-Deno.serve(async (req) => {
+// Initialize Supabase client
+const supabase = createClient(Deno.env.get('SUPABASE_URL') as string, Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') as string);
+
+Deno.serve(async (request: Request) => {
   const signature = request.headers.get('Stripe-Signature');
   const body = await request.text();
 
@@ -27,9 +31,24 @@ Deno.serve(async (req) => {
     );
 
     console.log('[success] Received Stripe webhook event: ', receivedEvent);
+
+    if (receivedEvent.type === 'payment_intent.succeeded') {
+      const paymentIntent = receivedEvent.data.object;
+      const paymentId = paymentIntent.id;
+
+      const { data, error } = await supabase.from('payments').update({ status: 'succeeded' }).eq('payment_id', paymentId);
+
+      if (error) {
+        console.error('Error updating payment status: ', error);
+        return new Response(error.message, { status: 500 });
+      }
+
+      console.log('Payment status updated successfully: ', data);
+    }
+
   } catch (error) {
     console.log('[error] Could not handle Stripe webhook event: ', error); 
-    return new Response(err.message, { status: 400 });
+    return new Response(error.message, { status: 400 });
   }
 
   console.log(`Event received: ${receivedEvent.id}`);
