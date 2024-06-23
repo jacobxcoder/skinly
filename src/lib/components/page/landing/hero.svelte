a6ea13e73870b52a588b85c6b642372dce83a997
jacobxcoder <gillbatesdev@gmail.com>
2024-06-23 21:09:38 +0200

    feat(landing): init hero section

---
+++ src/lib/components/page/landing/hero.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<section id="hero">
+  <div class="mx-auto max-w-xl text-center">
+    <span class="mb-4 block text-sm font-semibold uppercase tracking-wide text-primary">
+      Tired of dermatologists and expensive skincare?
+    </span>
+
+    <h1 class="mb-4 text-4xl font-bold">Get rid of acne from inside, for good</h1>
+
+    <p class="text-base-content-secondary">
+      Tired of being prescribed with Accutane and a long list of topicals which do not
+      work? Your doctors are not telling you
+      <span class="font-bold"> treat the root cause of acne </span>. This is the roadmap
+      to clean your skin from inside out.
+    </p>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<div>Hello!</div>
+<script lang="ts">
+  import HeroSection from '$lib/components/page/landing/hero.svelte';
+</script>
+
+<div>
+  <HeroSection />
+</div>
