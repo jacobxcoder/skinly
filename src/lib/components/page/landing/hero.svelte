94ea705b4f4426e9bccd27d0e32a3406e2fbc054
jacobxcoder <gillbatesdev@gmail.com>
2024-06-23 21:48:46 +0200

    feat(landing): improve hero with buttons

--- src/lib/components/page/landing/hero.svelte
+++ src/lib/components/page/landing/hero.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
+  import { Button } from '$lib/components';
 </script>
 
-<section id="hero">
-  <div class="mx-auto max-w-xl text-center">
+<section id="hero" class="py-12">
+  <div class="mx-auto text-center">
     <span class="mb-4 block text-sm font-semibold uppercase tracking-wide text-primary">
       Tired of dermatologists and expensive skincare?
     </span>
 
-    <h1 class="mb-4 text-4xl font-bold">Get rid of acne from inside, for good</h1>
+    <h1 class="mx-auto mb-6 max-w-3xl text-center text-5xl font-bold">
+      Get rid of acne for good by treating the root cause
+    </h1>
 
-    <p class="text-base-content-secondary">
-      Tired of being prescribed with Accutane and a long list of topicals which do not
-      work? Your doctors are not telling you
-      <span class="font-bold"> treat the root cause of acne </span>. This is the roadmap
-      to clean your skin from inside out.
+    <p class="text-base-content-secondary mx-auto mb-8 max-w-xl">
+      Accutane causes dry skin, organ disfunction, muscle cramps, hair loss, and
+      infertility. In the long run, Accutane and expensive topicals will not heal you. You
+      can only heal yourself from the inside.
     </p>
+  </div>
+
+  <div class="flex items-center justify-center gap-2">
+    <Button class="btn-outline border-base-300">What is Skinly?</Button>
+    <Button class="btn-primary">Get access</Button>
   </div>
 </section>
