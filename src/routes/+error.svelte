cedaf2b7e17001deb7701cbe63d9ccc3f6cf736e
jacobxcoder <gillbatesdev@gmail.com>
2024-06-26 09:21:24 +0200

    feat(landing): add a 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+  <div class="text-center">
+    <p class="text-base font-semibold text-primary">404</p>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page not found</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600">
+      Sorry, we couldn’t find the page you’re looking for.
+    </p>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+      <a href="/" class="btn btn-primary"> Go back home </a>
+      <a href="/support" class="text-sm font-semibold">
+        Contact support <span aria-hidden="true">&rarr;</span>
+      </a>
+    </div>
+  </div>
+</main>
